49e6f32a964e5b10797333f6d8e1bc6e19afb18e
HeberMatta <mattaheber8@gmail.com>
2026-02-19 00:45:33 -0300

    Add Treasure Island game implementation

---
+++ TreasureIslandProyect.py
@@ -0,0 +1,38 @@
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+print("Where do you want to go?")
+if "left" == input("Type left or right ").lower():
+    print("You've come to a beach and there is an island far away!. Ohh!!, there is a boat coming to you")
+    if "wait" == input("Type wait or swim").lower():
+        print("You arrive at the island unharmed. There are three bottles, but you can only pick one!")
+        number = input("Type '1', '2' or '3' to select a bottle")
+        if "1" == number:
+            print("You find the map of the treasure!!!, you find the treasure!")
+        elif "2" == number:
+            print("You find a map to the town. Game Loss!!")
+        else: print("You find a map to another island. Game Loss!!")
+    else: print("You start swimming and a shark bite you. Game Loss!!")
+else: print("You get lost. Game Loss!!")
