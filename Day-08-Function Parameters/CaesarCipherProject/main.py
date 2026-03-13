9c2f6304d4af73328aa677b5ebcc359f2975894e
HeberMatta <mattaheber8@gmail.com>
2026-03-12 22:32:07 -0300

    Implement Caesar cipher encoding and decoding

---
+++ Day-08-Function Parameters/CaesarCipherProject/main.py
@@ -0,0 +1,30 @@
+from art import logo
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+continue_flag = True
+
+def cipher(text, shift, selection):
+    output_text = ""
+    if selection == 'd':
+        shift *= -1
+    for char in text:
+        if char in alphabet:
+            index = (alphabet.index(char) + shift)%27
+            output_text += alphabet[index]
+        else:
+            output_text += char
+    print(f"The result is: {output_text}")
+
+print(logo)
+input("Press Enter to continue...")
+
+while continue_flag:
+    selection = input("Enter 'd' for decode or 'e' for encode: ")
+    text = input("Enter the text to be encrypted: ").lower()
+    shift = int(input("Enter the shift number: "))
+
+    cipher(text, shift, selection)
+
+    if input("Do you want to encode or decode again?(y/n)") == "y":
+        continue_flag = True
+    else:
+        continue_flag = False
