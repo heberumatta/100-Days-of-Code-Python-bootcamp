06c257a85f57d751e6f513a041908be5b59153d7
HeberMatta <mattaheber8@gmail.com>
2026-03-14 20:49:25 -0300

    Add practice comment to task.py

---
+++ Day-10-FunctionsWithOutputs/FunctionsWithOutputs/task.py
@@ -0,0 +1,12 @@
+#Practice
+def format_name(f_name, l_name):
+    modify_name = f_name.title()
+    modify_l_name = l_name.title()
+    return modify_name, modify_l_name
+
+print(format_name("heber", "matta"))
+
+#We can save the output
+
+nameTitel = format_name("heber", "matta")
+print(nameTitel)
