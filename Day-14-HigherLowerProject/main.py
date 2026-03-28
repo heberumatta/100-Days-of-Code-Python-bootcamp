e1b0670c61a363704ab42c94da915f01fd3d249a
heberumatta <matteheber8@gmail.com>
2026-03-28 17:25:50 -0300

    Fix comparison logic and improve input handling in Higher Lower game

--- Day-14-HigherLowerProject/main.py
+++ Day-14-HigherLowerProject/main.py
@@ -17,7 +17,7 @@ def print_comparison(account_a, account_b):
 
 def compare(account_a, account_b):
     """Compare the follower count of two accounts."""
-    if account_a['follower_count'] > account_b['follower_count']:
+    if account_a['follower_count'] >= account_b['follower_count']:
         return 'a'
     else:
         return 'b'
@@ -35,13 +35,16 @@ print(logo)
 
 while game_should_continue:
     print_comparison(account_a, account_b)
-    while guess := input("Who has more followers? Type 'A' or 'B': ").lower() not in ['a', 'b']:
+    while (guess := input("Who has more followers? Type 'A' or 'B': ").lower()) not in ['a', 'b']:
         print("Invalid input. Please type 'A' or 'B'.")
     if guess == compare(account_a, account_b):
         score += 1
+        print("\n" + "-"*50 + "\n")
         print(f"You're right! Current score: {score}.")
         account_a = account_b
         account_b = data.pop()
     else:
         game_should_continue = False
+        print("\n" + "-"*50 + "\n")
         print(f"Sorry, that's wrong. Final score: {score}.")
+    print("\n" + "-"*50 + "\n")
