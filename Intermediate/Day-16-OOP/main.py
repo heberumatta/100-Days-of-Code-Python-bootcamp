b9bef6b039df682032382544d02920c3a33a7976
HeberMatta <mattaheber8@gmail.com>
2026-04-19 23:26:07 -0300

    Refactor turtle graphics code and add prettytable for data representation

--- Intermediate/Day-16-OOP/main.py
+++ Intermediate/Day-16-OOP/main.py
@@ -1,13 +1,24 @@
 import turtle as t
 import random
 
-jim = t.Turtle()
-jim.shape("turtle")
-jim.color("blue")
-for steps in range(100):
-    jim.forward(random.randint(1, 50))
-    jim.right(random.randint(1, 360))
+#jim = t.Turtle()
+#jim.shape("turtle")
+#jim.color("blue")
+#for steps in range(100):
+#    jim.forward(random.randint(1, 50))
+#    jim.right(random.randint(1, 360))
 
 
-my_screen = t.Screen()
-my_screen.exitonclick()
+#my_screen = t.Screen()
+#my_screen.exitonclick()
+
+import prettytable as pt
+
+table = pt.PrettyTable()
+table.add_column("Kilo", [1,'','','','',''])
+table.add_column("Deca", [1,0,'','','',''])
+table.add_column("Hecto", [1,0,0,'','',''])
+table.add_column("Unit", [1,0,0,0,'',''])
+table.add_column("Deci", [1,0,0,0,0,''])
+table.add_column("Centi", [1,0,0,0,0,0])
+print(table)
