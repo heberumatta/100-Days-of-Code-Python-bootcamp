383084d5a5de4f5bb59b0764bee25466a6b3c7fe
HeberMatta <mattaheber8@gmail.com>
2026-05-03 15:09:33 -0300

    Add Hirst painting and color utilities
    Add image.jpg and color utilities to support new drawing modes: introduce colorgram-based extract_colors, random_color, and set colormode(255). Add new drawing functions (randomwalk, spirograph, hirstPainting) and switch main call to hirstPainting(tt). Also adjust turtle speed to "fastest" and tweak pensize/positions in differentShapes; use the bundled image.jpg as the color source for the Hirst-style dot painting.

--- Intermediate/Day-18-Turtle-GUI/turtlecode.py
+++ Intermediate/Day-18-Turtle-GUI/turtlecode.py
@@ -1,7 +1,24 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
+import random
+import colorgram
+
+def extract_colors(image_path, num_colors):
+    rgb_colors = []
+    colors = colorgram.extract('image.jpg', 30)
+    for color in colors:
+        rgb_colors.append(color.rgb)
+    return rgb_colors
+
+colormode(255)
+
+def random_color():
+	r = random.randint(0,255)
+	g = random.randint(0,255)
+	b = random.randint(0,255)
+	return (r,g,b)
 
 tt = Turtle()
-tt.speed(2)
+tt.speed("fastest")
 
 def square(t):
 	t.pensize(5)
@@ -65,20 +82,51 @@ def square(t):
 	t.setpos(1000,0)
 
 def differentShapes(t):
-	t.pensize(5)
+	t.pensize(3)
 	t.penup()
-	t.setpos(-25,25)
+	t.setpos(-25,500)
 	t.pd()
-	colors = ["red","blue","green","yellow","purple","orange","cyan","magenta","black"]
-	for i in range(4,10):
-		t.color(colors.pop())
+	colors ="#000000"
+	for i in range(3,20):
+		t.color(colors)
 		for j in range(i):
-			t.fd(100)
-			t.rt(360//i)
+			t.fd(50)
+			t.rt(360/i)
+
+def randomwalk(t):
+	t.pensize(15)
+	for i in range(200):
+		t.color(random_color())
+		t.fd(30)
+
+		t.setheading(random.choice([0,90,180,270]))
+
+def spirograph(t):
+	t.pensize(1)
+	for i in range(1,360,5):
+		t.color(random_color())
+		t.circle(100)
+		t.setheading(i)
+
+def hirstPainting(t):
+	t.penup()
+	t.setpos(-250,-250)
+	t.pd()
+	colors = extract_colors('image.jpg', 30)
+	for i in range(10):
+		for j in range(10):
+			t.color(random.choice(colors))
+			t.dot(20)
+			t.penup()
+			t.fd(50)
+		t.bk(500)
+		t.lt(90)
+		t.fd(50)
+		t.rt(90)
 
 
 
-differentShapes(tt)
+hirstPainting(tt)
 
 screen = Screen()
 screen.exitonclick()
