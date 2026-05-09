760b49dd4caf2147315e5e8d3df600a146da84d3
HeberMatta <mattaheber8@gmail.com>
2026-05-09 19:21:50 -0300

    backup 19:21-09052026

--- Intermediate/Day-20-Animation-Coordinates/Snake-Game/main.py
+++ Intermediate/Day-20-Animation-Coordinates/Snake-Game/main.py
@@ -1,13 +1,20 @@
 from turtle import Turtle, Screen
+from snake import Snake
 
-sc = Screen()
-sc.screensize(600,600,"black")
-sc.addshape("cube",((0,0), (5,0), (5,5), (0,5)))
-t = Turtle()
-t.shape("cube")
-t.color("white")
-t2 = Turtle()
-t2.shape("cube")
-t2.color("white")
-t.goto((t2.pos())+(5,0))
-sc.exitonclick()
+Screen = Screen()
+Screen.setup(width=600, height=600)
+Screen.bgcolor("black")
+Screen.title("Snake Game")
+
+snake = Snake()
+Screen.ontimer(snake.generaApple, 1)
+Screen.listen()
+Screen.onkey(snake.up, "Up")
+Screen.onkey(snake.down, "Down")
+Screen.onkey(snake.left, "Left")
+Screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    Screen.update()
+    snake.move()
---
+++ Intermediate/Day-20-Animation-Coordinates/Snake-Game/snake.py
@@ -0,0 +1,81 @@
+from turtle import Turtle,Screen
+import random
+screen = Screen()
+screen.addshape("cube", ((-5, -5), (-5, 5), (5, 5), (5, -5)))
+silueta_manzana = (
+    (5, 0),    # Punta superior (tallo)
+    (7, 2),    # Parte superior derecha
+    (8, 4),    # Costado derecho alto
+    (7, 7),    # Costado derecho bajo
+    (5, 9),    # Fondo derecho
+    (3, 7),    # Costado izquierdo bajo
+    (2, 4),    # Costado izquierdo alto
+    (3, 2),    # Parte superior izquierda
+    (5, 0)     # Volver al inicio
+)
+screen.addshape("apple", silueta_manzana)
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.apples = []
+        self.create_snake()
+
+    def create_snake(self):
+        for i in range(10):
+            self.add_segment((-10 * i, 0))
+
+    def add_segment(self, position):
+        new_segment = Turtle("cube")
+        new_segment.color("white")
+        new_segment.speed(1)
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+    
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+    
+    def move(self):
+        for seg in range(len(self.segments)-1, -1, -1):
+            if seg == 0:
+                self.segments[0].forward(10)
+            else:
+                new_x = self.segments[seg-1].xcor()
+                new_y = self.segments[seg-1].ycor()
+                self.segments[seg].goto(new_x, new_y)
+
+    def up(self):
+        if self.segments[0].heading() != 270:
+            self.segments[0].speed(10)
+            self.segments[0].setheading(90)
+            self.segments[0].speed(1)
+        
+    def down(self):
+        if self.segments[0].heading() != 90:
+            self.segments[0].speed(10)
+            self.segments[0].setheading(270)
+            self.segments[0].speed(1)
+    
+    def left(self):
+        if self.segments[0].heading() != 0:
+            self.segments[0].speed(10)
+            self.segments[0].setheading(180)
+            self.segments[0].speed(1)
+    
+    def right(self):
+        if self.segments[0].heading() != 180:
+            self.segments[0].speed(10)
+            self.segments[0].setheading(0)
+            self.segments[0].speed(1)
+
+    def generaApple(self):
+        cor_x = random.randint(-300, 300)
+        cor_y = random.randint(-300, 300)
+        new_apple = Turtle("apple")
+        new_apple.color("white")
+        new_apple.penup()
+        new_apple.goto(cor_x, cor_y)
+        self.apples.append(new_apple)
+        screen.ontimer(self.generaApple, 5000)
+        
